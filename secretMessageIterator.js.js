de57408955d92f89079a975f399609404752a36d
DayMaker26 <34576518+DayMaker26@users.noreply.github.com>
2018-04-04 14:55:29 -0500

    Add files via upload

---
+++ secretMessageIterator.js.js
@@ -0,0 +1,11 @@
+let animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+
+let secretMessage = animals.map(animals => animals [0]);
+
+console.log(secretMessage.join(''));
+
+let bigNumbers = [100, 200, 300, 400, 500];
+
+
+let smallNumbers = bigNumbers.map(bigNumbers => bigNumbers / 100);
