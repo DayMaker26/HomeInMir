4b48b86c2a4631410dfea05fde77e07e26e72881
DayMaker26 <34576518+DayMaker26@users.noreply.github.com>
2018-04-08 00:06:59 -0500

    Add files via upload

---
+++ myPlaces.js
@@ -0,0 +1,11 @@
+let myPlaces = ['Avalon', 'StoneHedge ', 'Anglessey'];
+
+let friendPlaces = ['Avalon', 'Texas', 'Mexico'];
+
+for (let myPlacesIndex = 0; myPlacesIndex < myPlaces.length; myPlacesIndex++) {
+  for (let friendPlacesIndex = 0; friendPlacesIndex < friendPlaces.length; friendPlacesIndex++) {
+		if (myPlaces[myPlacesIndex] === friendPlaces[friendPlacesIndex]) {
+      console.log(friendPlaces[friendPlacesIndex]);
+    }
+  }
+}
